f84292370beb36aefd9412e120f7f10075282e7a
carlos <carlosveizaga.jcvc@gmail.com>
2019-12-04 21:07:48 -0400

    make it better

--- src/classifyController.js
+++ src/classifyController.js
@@ -1,27 +1,63 @@
 import BrainJSClassifier from 'natural-brain'
 import natural from 'natural';
 
-let classifier = new natural.BayesClassifier();
+//var classifier = new BrainJSClassifier();
+
+var classifier = new natural.BayesClassifier();
 
 const train = (request, response) => {
     let { name, description } = request.body
 
+    if (!description || /^\s*$/.test(description)) {
+        response.status(400).json({'error' : "OK"})        
+    }
     classifier.addDocument(description, name)
 
     console.log("name => ",  name, "desc => ",  description)
     
     classifier.train()
-    //classifier.save('./test/brain_classifier.json', function () {});
+    classifier.save('test/brain_classifier.json', function () {});
 
-    response.status(200).json([{'trained' : "OK"}])
+    response.status(200).json({'trained' : "OK"})
 }
 
 const ClassifyAreas = (request, response) => {
     let { content } = request.body
     
+    let classifications = classifier.getClassifications(content);
+    
+    if (classifications.length == 0) {
+        response.status(204).json({'trained' : "NOT TRAINED"})        
+    }
+
     let category =  classifyText(content)
 
     response.status(200).json(category)
+}
+
+const ClassifyAreasTest = (request, response) => {
+ 
+ BrainJSClassifier.load('brain_classifier.json', function (err, classifier) {})
+    //classifier.addDocument("Mostrar nuestro sentimiento a una persona , llevarle serenatas es romance", "Amor")
+    //classifier.addDocument("Tu eres mi alma genemela por eso te quiero", "Amor")
+    classifier.addDocument("Tu eres el amor de mi vida", "Amor")
+    classifier.addDocument("El futbol es uno de los deportes mas practicados del mundo", "Deportes")
+    classifier.addDocument("La economia es la ciencia que estudia los recursos, la creación de riqueza y la producción, distribución y consumo de bienes y servicios, para satisfacer las necesidades humanas", "Economia")
+    classifier.addDocument("Los filosofos hacen que nuestro cebrero se confunda", "Filosofia")
+    classifier.train()
+
+    let text = "messi es el mejor jugador , tiene una velocidad impresionante y un regate espectacular."
+    let text2 = "todos lo pobres sufren por la falta de dinero."
+    let text3 = "Aristoteles fue un pensador que colaboro con frases locas."
+    let text4 = "Federer es un buen tenista."
+
+    //classifier.save('brain_classifier.json', function (err, classifier) {});
+    let category =  classifyText(text)
+    let category2 =  classifyText(text2)
+    let category3 =  classifyText(text3)
+    let category4 =  classifyText(text4)
+
+    response.status(200).json([{text: category, text2: category2, text3: category3, text4: category4}])
 }
 
 const classifyText = (text) => {
@@ -36,4 +72,5 @@ const classifyText = (text) => {
 module.exports = {
     ClassifyAreas,
     train,
+    ClassifyAreasTest
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import classify from './classifyController'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
 
+app.use(cors({
+	origin: 'http://localhost:8000'
+}))
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
-      extended: true,
+      extended: false,
     })
 )
 
 app.post('/api/v1/train', classify.train)
 app.post('/api/v1/analize', classify.ClassifyAreas)
+app.get('/api/v1/test', classify.ClassifyAreasTest)
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
